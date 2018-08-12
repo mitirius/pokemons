cef055372cafa0371f59bd0ff91142381b65f4a8
mitil <mitirius@gmail.com>
2018-08-12 16:14:07 +0200

    Updated task.

--- src/components/EvolutionList.tsx
+++ src/components/EvolutionList.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import { EVOL_URL } from '../App';
 import { match } from 'react-router-dom';
 import './EvolutionList.css';
+import SpeciesItem from './SpeciesItem';
+import { Species } from './SpeciesList';
 
 interface Props {
   match: match<{ id: number }>;
 }
 
 interface State {
-  evoList: any[];
+  evoList: Species[];
 }
 
 export default class EvolutionList extends React.PureComponent<Props, State> {
@@ -31,23 +33,28 @@ export default class EvolutionList extends React.PureComponent<Props, State> {
       .then(res => res.json())
       .then(
         json =>
-          this.isActive && this.setState({ evoList: json.chain.evolves_to })
+          this.isActive &&
+          this.setState({ evoList: this.getEvolist(json.chain, []) })
       )
       .catch(e => console.log(e));
   };
 
-  renderItem = (item: { species: { name: string } }, idx: number) => {
-    return (
-      <li className="evo-list__item item" key={idx}>
-        <h4 className="item__name">{item.species.name}</h4>
-        {console.log(item)}
-      </li>
-    );
-  };
+  getEvolist(json: any, array: Species[]): Species[] {
+    array.push(json.species);
+    if (json.evolves_to.length === 0) {
+      return array;
+    } else {
+      return this.getEvolist(json.evolves_to[0], array);
+    }
+  }
 
   render() {
     return (
-      <ul className="evo-list">{this.state.evoList.map(this.renderItem)}</ul>
+      <ul className="evo-list">
+        {this.state.evoList.map((item, idx) => (
+          <SpeciesItem key={idx} species={item} />
+        ))}
+      </ul>
     );
   }
 }
